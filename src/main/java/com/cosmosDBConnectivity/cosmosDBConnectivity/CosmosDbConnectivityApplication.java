10f3e372c09b13ddd2307cf74dd5e1296663645c
Venkataraman S B <svenka65@ford.com>
2019-12-02 18:56:23 +0530

    repo commit

---
+++ src/main/java/com/cosmosDBConnectivity/cosmosDBConnectivity/CosmosDbConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.cosmosDBConnectivity.cosmosDBConnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosDbConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosDbConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmosDBConnectivity/cosmosDBConnectivity/CosmosDbConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmosDBConnectivity.cosmosDBConnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosDbConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
